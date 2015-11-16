72383c49cc9b9aff810c9825444c371d1399b1b9
aclergeot <anthony.clergeot.sio@gmail.com>
2015-11-16 17:25:32 +0100

    C5

--- src/Garagiste.java
+++ src/Garagiste.java
@@ -9,5 +9,21 @@ public class Garagiste {
 		this.nom = nom;
 		this.adresse = adresse;
 	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
 	
 }
